71efb249e455e9520616f2579117de2354727ee9
123 <123>
2024-03-01 11:37:45 +0530

    Spring batch app

---
+++ SpringBatchDemo/src/main/groovy/com/example/springbatch/SpringBatchDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.springbatch
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBatchDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringBatchDemoApplication, args)
+	}
+
+}
